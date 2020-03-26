11b46fd40d710ac9d4744ad94d695d8f656999de
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 11:56:51 -0700

    :bug: addinng propTypes to Rooms view component

--- src/views/Rooms.js
+++ src/views/Rooms.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import styled from '@styling';
 
 import Button from 'Components/Button';
 import Link from 'Components/Link';
 import { Switch, Route } from 'Components/Router';
 
+import CreateRoom from 'Views/CreateRoom';
+
 const Actions = styled('aside')``;
 
-const Rooms = ({className, match: { url } }) => (
+const Rooms = ({ className, match: { url } }) => (
   <div className={className}>
     <Switch>
-      <Route path={`${url}/add`} component={() => <h1>dfd</h1>} />
+      <Route component={CreateRoom} path={`${url}/create`} />
       <Route path="/">
         <Actions>
-          <Button to={`${url}/add`} as={Link}>Add</Button>
+          <Button as={Link} to={`${url}/create`}>
+            Create room
+          </Button>
         </Actions>
       </Route>
     </Switch>
   </div>
-)
+);
 
-export default styled(Rooms)``;
+Rooms.propTypes = {
+  className: PropTypes.string,
+  match: PropTypes.shape({
+    url: PropTypes.string
+  })
+}
+
+export default styled(Rooms)`
+  padding: 3.2rem;
+
+  ${Actions} {
+    text-align: right;
+  }
+`;
