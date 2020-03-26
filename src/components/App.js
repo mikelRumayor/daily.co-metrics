588db193208af6a48de331dd82f90c6bfc42efdd
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 10:11:14 -0700

    :sparkles: adding Rooms test component in the app

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import React from 'react';
 import Router, { Redirect, Route } from 'Components/Router';
 import Theme from 'Components/Theme';
 
+import Rooms from 'Views/Rooms';
+
 const App = () => (
   <Router>
     <Theme>
-    <Route exact path="/">
-      <Redirect to="/rooms" />
-    </Route>
-      <Route component={() =>  <h1>Daily.js</h1>} path="/rooms" />
+      <Route exact path="/">
+        <Redirect to="/rooms" />
+      </Route>
+      <Route component={Rooms} path="/rooms" />
     </Theme>
   </Router>
-)
+);
 
-export default App
+export default App;
