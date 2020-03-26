e75dbf1f10c298a5895f672ae75c562c5ced3a08
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 09:34:12 -0700

    :lipstick: creating theme for styling

---
+++ src/theme/fonts.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import { css } from '@styled-components';
+
+export default {
+  '--font-large': css`
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+  `,
+  '--font-medium': css`
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+  `,
+  '--font-small': css`
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+  `,
+
+  '--font-weight-book': css`
+    font-weight: 400;
+  `,
+  '--font-weight-medium': css`
+    font-weight: 500;
+  `,
+  '--font-weight-demi': css`
+    font-weight: 600;
+  `,
+};
