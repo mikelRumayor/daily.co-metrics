2ead35aea5820aeac7a4a60ba58053e978d1e819
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 10:49:54 -0700

    :sparkles: adding dumb header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styled, { theme } from '@styling';
+
+const Title = styled('h1')``;
+
+const Header = ({ className }) => (
+  <header className={className}>
+    <Title>Daily.js</Title>
+  </header>
+)
+
+Header.propTypes = {
+  className: PropTypes.string
+}
+
+export default styled(Header)`  
+  ${theme('--font-large')};
+  align-items: center;
+  background: ${theme('--color-primary')};
+  color: ${theme('--color-light')};
+  display: flex; 
+  height: 6.4rem;
+  padding: 0 3.2rem;
+`;
