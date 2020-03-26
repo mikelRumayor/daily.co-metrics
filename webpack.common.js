79cc1d65aca05238e90723cf04cfccde71a0e714
Mikel Rumayor <mikel@joinedapp.com>
2020-03-25 18:31:08 -0700

    :wrench: adding babel-loader configuration for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,13 @@ const path = require('path');
 
 module.exports = {
   module: {
-
+    rules: [
+      {
+        exclude: [path.resolve(__dirname, 'node_modules/')],
+        test: /\.js$/,
+        use: ['babel-loader'],
+      },
+    ]
   },
   output: {
     
