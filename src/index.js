dcfa0902c3e6083eb030f9dff3fd717dee5d39b0
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 09:03:45 -0700

    :wrench: adding react-hot-loader configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { setConfig } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 
-import App from 'Components/App'
+import App from 'Components/App';
 
-render(<App />, document.getElementById('app'));
+setConfig({
+  reloadHooks: false,
+});
+
+const Root = hot(App);
+
+render(<Root />, document.getElementById('app'));
