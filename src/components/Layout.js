5a190401bed0520dd60ae4c36c05203f42656b4f
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 10:51:00 -0700

    :sparkles: adding simple layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styled from '@styling';
+
+import Header from 'Components/Header';
+import Sidebar from 'Components/Sidebar';
+
+const Main = styled('main')``;
+
+const Layout = ({ className, children }) => (
+  <section className={className}>
+    <Header/>
+    <Sidebar/>
+    <Main>
+      {children}
+    </Main>
+  </section>
+)
+
+Layout.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+}
+
+export default styled(Layout)`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+
+  ${Header} {
+    grid-row: 1;
+    grid-column: 1 / span 12;
+  }
+
+  ${Sidebar} {
+    grid-row: 2;
+    grid-column: 1 / span 3;
+    height: calc(100vh - 6.4rem);
+  }
+`;
