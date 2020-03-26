64d22ee9323c01911401f17ba20beed9440c5694
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 10:50:37 -0700

    :sparkles: adding simple Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styled, { theme } from '@styling';
+
+const Sidebar = ({ className }) => (
+  <nav className={className}>
+    
+  </nav>
+)
+
+Sidebar.propTypes = {
+  className: PropTypes.string
+}
+
+export default styled(Sidebar)`  
+ border: 0.1rem solid ${theme('--color-main')};
+`;
