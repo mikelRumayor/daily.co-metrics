b80f212a4529bfd8c3950f0791934fd13a766e92
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 15:53:03 -0700

    :recycle: addin propTypes to live view

--- src/views/Live.js
+++ src/views/Live.js
@@ -2,37 +2,42 @@ import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@styling';
 
-import Video from 'Providers/Video'
+import Video from 'Providers/video';
 import Services from 'Services/rooms';
 
-const Live = ({ className, match: { params: { id } } }) => {
+const Live = ({
+  className,
+  match: {
+    params: { id },
+  },
+}) => {
   const element = useRef();
   const frame = useRef();
 
   useEffect(() => {
     (async () => {
-      const { url } = await Services.getById(id)
-      frame.current = Video.wrap(element.current, { })
-      frame.current.join({ url })
-    })()
-  }, [id, element])
+      const { url } = await Services.getById(id);
+      frame.current = Video.wrap(element.current, {});
+      frame.current.join({ url });
+    })();
+  }, [id, element]);
 
-  // setInterval(async () => {
-  //   if(frame.current) {
-  //     const a = await frame.current.getNetworkStats()
-  //     console.log(a)
-  //   }
-  // }, 4000)
-   
   return (
-    <iframe className={className} ref={element}  allow="microphone; camera; autoplay"/>
-  )
-}
+    <iframe
+      ref={element}
+      allow="microphone; camera; autoplay"
+      className={className}
+      title="live video"
+    />
+  );
+};
 
 Live.propTypes = {
   className: PropTypes.string,
   match: PropTypes.shape({
-    url: PropTypes.string,
+    params: PropTypes.shape({
+      id: PropTypes.string
+    })
   }),
 };
 
