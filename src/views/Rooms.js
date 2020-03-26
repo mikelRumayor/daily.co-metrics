600f21d483edcca0935fb4ba6f0feb3288cb8b65
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 11:17:42 -0700

    :recycle: adding nested route for adding rooms

--- src/views/Rooms.js
+++ src/views/Rooms.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import styled from '@styling';
 
-const Rooms = () => <h1>rooms</h1>;
+import Button from 'Components/Button';
+import Link from 'Components/Link';
+import { Switch, Route } from 'Components/Router';
 
-export default Rooms;
+const Actions = styled('aside')``;
+
+const Rooms = ({className, match: { url } }) => (
+  <div className={className}>
+    <Switch>
+      <Route path={`${url}/add`} component={() => <h1>dfd</h1>} />
+      <Route path="/">
+        <Actions>
+          <Button to={`${url}/add`} as={Link}>Add</Button>
+        </Actions>
+      </Route>
+    </Switch>
+  </div>
+)
+
+export default styled(Rooms)``;
