9ca471e6ecfb78b71a3d8449de04dc6a53af7bef
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 12:32:02 -0700

    :recycle: adding form managenet code for input

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { theme } from '@styling';
-import { v4 as uuid } from 'uuid';
+import styled from '@styling';
 
-const Label = styled('label')``;
-const Field = styled('input')``;
+import Select from './Select'
+import Text from './Text'
 
-const Input = ({ className, label, id = uuid(), ...rest }) => (
-  <div className={className}>
-    <Label htmlFor={id}>{label}</Label>
-    <Field {...rest} />
-  </div>
-);
+const TYPES = {
+  select: Select,
+  text: Text
+}
 
-Input.propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.string,
-  label: PropTypes.string,
+const Input = ({ type = 'text', ...props }) => {
+  const Element = TYPES[type];
+
+  return (
+    <Element {...props} />
+  )
 };
 
-export default styled(Input)`
-  ${theme('--font-medium')}
-  display: flex;
+Input.propTypes = {
+  type: PropTypes.string,
+};
 
-  ${Label} {
-    margin-right: 0.8rem;
-    text-transform: capitalize;
-  }
-
-  ${Field} {
-    border-radius: 0.4rem;
-    padding: 0.4rem 0.8rem;
-    text-transform: capitalize;
-  }
-`;
+export default styled(Input)``;
