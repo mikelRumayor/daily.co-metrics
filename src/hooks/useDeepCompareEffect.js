b0e41e7d5d9725b276df1664a12e0bf6727890a7
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 17:32:35 -0700

    :sparkles: adding deep dependency comparison hook

---
+++ src/hooks/useDeepCompareEffect.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+import useDeepCompareMemoize from './useDeepCompareMemoize'
+
+export function useDeepCompareEffect(callback, dependencies) {
+  return useEffect(callback, useDeepCompareMemoize(dependencies));
+}
+
+export default useDeepCompareEffect
