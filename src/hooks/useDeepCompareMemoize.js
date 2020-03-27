ada484817e878a8ba17f3ba4c52017c2d6533177
Mikel Rumayor <mikel@joinedapp.com>
2020-03-26 17:32:02 -0700

    :recycle: adding deep compare memoization hook

---
+++ src/hooks/useDeepCompareMemoize.js
@@ -0,0 +1,14 @@
+import { useRef } from 'react';
+import isEqual from 'lodash.isequal';
+
+function useDeepCompareMemoize(value) {
+  const ref = useRef();
+
+  if (!isEqual(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+export default useDeepCompareMemoize
