1a9ba55435a89a1f9fb79ca7b75a4e09d3e6bed8
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 10:45:51 -0700

    :sparkles: introducing rooms service

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -4,11 +4,16 @@ function create(values) {
   return rest.create('/rooms', values);
 }
 
+function get() {
+  return rest.read(`/rooms`);
+}
+
 function getById(id) {
   return rest.read(`/rooms/:${id}`);
 }
 
 export default {
   create,
+  get,
   getById,
 };
