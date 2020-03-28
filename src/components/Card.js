10d3b5d99ea566dfbb72a380c2a84127251e337a
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 10:47:12 -0700

    :sparkles: introducing Card component for rooms list

---
+++ src/components/Card.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled, { theme } from '@styling';
+
+import Link from 'Components/Link';
+
+const Date = styled('span')``
+const Name = styled('span')``
+const Privacy = styled('span')``
+
+const Card = ({ className, created_at, id, name, privacy, url }) => (
+  <div className={className}>
+    <Name>{name}</Name>
+    <Date>{new window.Date(created_at).toDateString()}</Date>
+    <Privacy>{privacy}</Privacy>
+    <Link to={`/rooms/${id}`}>go to stats</Link>
+    <Link to={`/live/${id}`}>go to call</Link>
+  </div>
+)
+
+export default styled(Card)`
+  box-shadow: 0 0.1rem 0.3rem 0 rgba(0,0,0,0.15);
+  border-radius: 0.5rem;
+  display: grid;
+  padding: 1.6rem 3.2rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 1.6rem 0.8rem;
+
+  ${Name} {
+    grid-row: 1;
+    grid-column: 1 / span 3;
+    ${theme('--font-large')};
+  }
+
+  ${Date} {
+    ${theme('--font-medium')};
+    grid-row: 1;
+    grid-column: 4 / span 3;
+  }
+
+  ${Privacy} {
+    ${theme('--font-medium')};
+    background: ${theme('--color-secondary')};
+    border-radius: 1.6rem;
+    color: ${theme('--color-light')};
+    grid-row: 2;
+    grid-column: 1;
+    max-width: 8rem;
+    text-align: center;
+    text-transform: capitalize;
+  }
+
+  ${Link} {
+    ${theme('--font-medium')};
+    grid-row: 3;
+    text-transform: capitalize;
+  }
+
+`;
