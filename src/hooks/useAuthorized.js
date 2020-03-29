1643c89ab391b6c629545c87667892a6328aadbb
Mikel Rumayor <mikel@joinedapp.com>
2020-03-29 12:12:57 -0700

    :sparkles: introducing useAuthorized hook for private calls

---
+++ src/hooks/useAuthorized.js
@@ -0,0 +1,15 @@
+import meetings from 'Services/meetings';
+import { useLocation } from 'Components/Router';
+
+import useFetcher from './useFetcher';
+
+const useAuthorized = () => {
+  const query = new URLSearchParams(useLocation().search);
+  const token = query.get('token')
+  const { data: { valid } = {}, loading } = useFetcher(meetings.validateToken, token);
+
+  if (loading || valid) return token;
+  return false;
+}
+
+export default useAuthorized;
