67780b7324a6e82bd9780da13a460f38fb03b19d
Mikel Rumayor <mikel@joinedapp.com>
2020-03-29 13:13:55 -0700

    :sparkles: introducing list component

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@styling';
+
+const List = ({ className, data = [], template: Template }) => (
+  <div className={className}>
+    {data.map(row => (
+      <Template key={row.id} {...row} />
+    ))}
+  </div>
+);
+
+List.propTypes = {
+  className: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.object),
+  template: PropTypes.node,
+};
+
+export default styled(List)``;
