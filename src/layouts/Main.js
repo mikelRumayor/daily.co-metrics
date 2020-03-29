5a453dde9d23efaf46ac446d79996f88c5ff8812
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 19:23:13 -0700

    :recycle: refactoring main layout

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types';
 import styled from '@styling';
 
 import Header from 'Components/Header';
-import Sidebar from 'Components/Sidebar';
 
 const Main = styled('main')``;
 
 const Layout = ({ className, children }) => (
   <section className={className}>
     <Header />
-    <Sidebar />
     <Main>{children}</Main>
   </section>
 );
@@ -32,14 +30,8 @@ export default styled(Layout)`
     grid-column: 1 / span 12;
   }
 
-  ${Sidebar} {
-    grid-row: 2;
-    grid-column: 1 / span 3;
-    height: calc(100vh - 6.4rem);
-  }
-
   ${Main} {
     grid-row: 2;
-    grid-column: 4 / span 9;
+    grid-column: 1 / span 12;
   }
 `;
