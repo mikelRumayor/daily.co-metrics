abe3baec5ab66ef3511b8ae8cc3efe428a868c47
Mikel Rumayor <mikel@joinedapp.com>
2020-03-29 12:14:27 -0700

    :recycle: introducing token validation

--- src/views/Live.js
+++ src/views/Live.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@styling';
 
+import useAuthorized from 'Hooks/useAuthorized';
 import useData from 'Hooks/useData';
 
 import StatProvider from 'Components/StatProvider';
+import Unauthorized from 'Components/Unauthorized';
 
 import rooms from 'Services/rooms';
 
@@ -15,18 +17,24 @@ const Live = ({
   },
 }) => {
   const data = useData(rooms.getById, id);
+  const token = useAuthorized()
+
   if (!data) return false;
+  const { url, privacy } = data;
 
-  const { url } = data;
+  if (!token && privacy === 'private') return <Unauthorized />
 
-  return <StatProvider className={className} id={id} url={url} />;
+  return <StatProvider className={className} id={id} url={token ? url : `${url}?t=${token}`} />;
 };
 
 Live.propTypes = {
   className: PropTypes.string,
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.string,
+      token: PropTypes.string,
+    }),
+    params: PropTypes.shape({
+      token: PropTypes.string,
     }),
   }),
 };
