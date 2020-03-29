87d8943494efc724f1610b0929dc93e660d5f96d
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 19:12:41 -0700

    :bug: fixing stat services

--- src/services/stats.js
+++ src/services/stats.js
@@ -1,9 +1,14 @@
 import rest from 'Providers/rest';
 
+function get(id) {
+  return rest.read(`/stats/${id}`);
+}
+
 function send(id, stats) {
   return rest.create(`/stats/${id}`, stats);
 }
 
 export default {
+  get,
   send,
 };
