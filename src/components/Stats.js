c35a7ecbc2199ac50a1a540d1c16b8fc4f0474d3
Mikel Rumayor <mikel@joinedapp.com>
2020-03-30 10:05:08 -0700

    :recycle: refactoring Stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,58 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@styling';
 
-import Chart from 'Components/Chart';
 import Loader from 'Components/Loader';
+import Participant from 'Components/Participant';
 
 import useFetcher from 'Hooks/useFetcher';
 
 import service from 'Services/stats';
 
-const Room = ({ className, id }) => {
-  const [{ data: stats, loading }] = useFetcher(service.get, id);
-
+const Stats = ({ className, id }) => {
+  const [{ data: participants, loading }] = useFetcher(service.get, id);
+  
   if (loading) return <Loader />;
-
-  const bitsPerSecond = stats.map(
-    ({ time, videoRecvBitsPerSecond, videoSendBitsPerSecond }) => ({
-      time,
-      videoRecvBitsPerSecond,
-      videoSendBitsPerSecond,
-    }),
-  );
-
-  const lostPackets = stats.map(
-    ({ time, videoRecvPacketLoss, videoSendPacketLoss }) => ({
-      time,
-      videoRecvPacketLoss,
-      videoSendPacketLoss,
-    }),
-  );
 
   return (
     <div className={className}>
-      <Chart
-        data={bitsPerSecond}
-        title="Bits per second"
-        yAxis="bits per second"
-      />
-      <Chart
-        data={lostPackets}
-        title="Packets transmision"
-        yAxis="packets lost"
-      />
+      {Object.keys(participants).map((participantId) => {
+        const bitsPerSecond = participants[participantId].map(
+          ({ time, videoRecvBitsPerSecond, videoSendBitsPerSecond }) => ({
+            time,
+            videoRecvBitsPerSecond,
+            videoSendBitsPerSecond,
+          }),
+        );
+
+        const lostPackets = participants[participantId].map(
+          ({ time, videoRecvPacketLoss, videoSendPacketLoss }) => ({
+            time,
+            videoRecvPacketLoss,
+            videoSendPacketLoss,
+          }),
+        );
+
+        return (
+          <Participant key={participantId} bitsPerSecond={bitsPerSecond} id={participantId} lostPackets={lostPackets} />
+        )
+      })}
     </div>
   );
 };
 
-Room.propTypes = {
+Stats.propTypes = {
   className: PropTypes.string,
   id: PropTypes.string,
 };
 
-export default styled(Room)`
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
+export default styled(Stats)``;
