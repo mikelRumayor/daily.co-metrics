5512e4e2d6daa355a4903f8eb6ac9008663c4e13
Mikel Rumayor <mikel@joinedapp.com>
2020-03-30 10:46:47 -0700

    :recycle: adding color alert to theme

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,7 +1,7 @@
 export default {
+  '--color-alert': '#ff0000',
   '--color-light': '#ffffff',
   '--color-main': '#000000',
   '--color-primary': '#1f2d3d',
   '--color-secondary': '#76c720',
-  '--color-alert': '#ff0000'
 };
